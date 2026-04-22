ae13b3e57e7d9af4d35789c5191e2e687985402e
marat <marat@hosteeva.com>
2026-04-22 22:33:38 +0400

    DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::query()->updateOrCreate([
+            'email' => 'admin@forlled.com',
+        ], [
+            'name' => 'Admin',
+            'password' => 'admin',
+            'email_verified_at' => now(),
         ]);
     }
 }
