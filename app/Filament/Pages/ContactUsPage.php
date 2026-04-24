bb5b8af34d14e5054bf238666d4202e8b37f81a9
marat <arzumanyanmarat711@gmail.com>
2026-04-24 23:31:25 +0400

    Contact Us Page

---
+++ app/Filament/Pages/ContactUsPage.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Actions\Action;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
+use Filament\Pages\Page;
+use App\Models\ContactUs as ContactUsModel;
+use Filament\Schemas\Components\Actions;
+use Filament\Schemas\Components\Component;
+use Filament\Schemas\Components\EmbeddedSchema;
+use Filament\Schemas\Components\Form;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+
+class ContactUsPage extends Page
+{
+    protected string $view = 'filament.pages.contact-us-page';
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-home';
+    protected static ?string $navigationLabel = 'Contact Us Page';
+
+    public ?ContactUsModel $record = null;
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->record = ContactUsModel::query()->firstOrCreate([]);
+        $this->form->fill($this->record->toArray());
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make()
+                    ->schema([
+                        TextInput::make('title')
+                            ->maxLength(255)
+                            ->columnSpanFull(),
+                        Textarea::make('description')
+                            ->rows(3)
+                            ->columnSpanFull(),
+                    ])
+            ])
+            ->statePath('data')
+            ->model($this->record);
+    }
+
+    public function save(): void
+    {
+        $data = $this->form->getState();
+
+        $this->record->fill($data);
+        $this->record->save();
+
+        Notification::make()
+            ->title('Contact us page saved')
+            ->success()
+            ->send();
+    }
+
+    public function content(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                $this->getFormContentComponent(),
+            ]);
+    }
+
+    protected function getFormContentComponent(): Component
+    {
+        return Form::make([EmbeddedSchema::make('form')])
+            ->id('form')
+            ->livewireSubmitHandler('save')
+            ->footer([
+                Actions::make([
+                    Action::make('save')
+                        ->label('Save')
+                        ->submit('save'),
+                ])->key('form-actions'),
+            ]);
+    }
+}
