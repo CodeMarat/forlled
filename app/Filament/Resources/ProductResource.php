cb23aec3862d7dc2436bf2f59cf6d6787180f187
marat_arzumanyan <marat@hosteeva.com>
2026-05-06 12:46:12 +0400

    Orders fix

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -21,6 +21,7 @@ use Filament\Tables\Table;
 class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
+    protected static ?int $navigationSort = -8;
 
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-rectangle-stack';
 
--- app/Filament/Resources/TreatmentResource.php
+++ app/Filament/Resources/TreatmentResource.php
@@ -22,6 +22,7 @@ use Filament\Tables\Table;
 class TreatmentResource extends Resource
 {
     protected static ?string $model = Treatment::class;
+    protected static ?int $navigationSort = -7;
 
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-rectangle-stack';
 
