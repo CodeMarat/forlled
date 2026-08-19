d44c5e0f13e9646c975d5bc6379e43a913dd4799
marat <arzumanyanmarat711@gmail.com>
2026-08-20 02:22:52 +0400

    Videos upload improvements

--- app/Jobs/OptimizeUploadedVideo.php
+++ app/Jobs/OptimizeUploadedVideo.php
@@ -2,10 +2,10 @@
 
 namespace App\Jobs;
 
-use Illuminate\Bus\Dispatchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\File;
